9907a6805445e4c8f54eea0a9fd7d80861f990f3
Sajeth Jonathan <sws.jona@gmail.com>
2020-08-22 11:56:51 +0000

    Adding initial Files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
